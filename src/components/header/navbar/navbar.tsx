f25aa4728d01dcd4937fed2e3f6aebc6f8276402
Vladislav Mironchuk <vlad.mironchuk.02@gmail.com>
2022-01-20 11:06:40 +0300

    add: add navbar

---
+++ src/components/header/navbar/navbar.tsx
@@ -0,0 +1,16 @@
+import "./navbar.scss";
+import { Link } from "../../../elements/navlink/navlink";
+import { Dropdown } from "@/elements/dropdown/dropdown";
+
+export const NavBar = () => {
+  return (
+    <nav className="header__nav">
+      <ul className="header__nav__ul">
+        <Link linkPath="/" linkText="Home" />
+        <Dropdown />
+        <Link linkPath="/about" linkText="About" />
+        <Link linkPath="/auth" linkText="Sign In" />
+      </ul>
+    </nav>
+  );
+};
