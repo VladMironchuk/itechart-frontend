4c8d5436ba35f852d64c6b1a639e4ba20fbc09d3
Vladislav Mironchuk <vlad.mironchuk.02@gmail.com>
2022-01-20 11:07:29 +0300

    add: add search bar

---
+++ src/elements/searchBar/searchBar.tsx
@@ -0,0 +1,59 @@
+import "./searchBar.scss";
+import { useHttp } from "@/helpers/useFetch";
+import { useState, useEffect } from "react";
+
+export const SearchBar: React.FC<{ name: string; placeholder: string }> = ({ name, placeholder }) => {
+  const [inputValue, setInputValue] = useState("");
+  const [{ games }, setGames] = useState<{ games: any[] }>({ games: [] });
+
+  const { isLoading, error, sendRequest } = useHttp();
+
+  let content;
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  useEffect(() => {
+    if (inputValue === "") {
+      setGames({ games: [] });
+      return;
+    }
+    console.log(inputValue);
+    sendRequest({ url: `/api/search/${inputValue}` }, setGames);
+    console.log(games);
+  }, [inputValue, sendRequest]);
+
+  return (
+    <div className="search-wrapper">
+      <input
+        onChange={(event) => {
+          setInputValue(event.target.value);
+        }}
+        value={inputValue}
+        className="search-bar"
+        type="text"
+        name={name}
+        placeholder={placeholder}
+      />
+      <div className="search-list">
+        <ul>
+          {games.map((game) => (
+            <li
+              onClick={() => {
+                alert("got product");
+              }}
+              key={game}
+            >
+              {game}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
