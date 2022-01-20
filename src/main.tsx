0ad36b475ea53be6d3845dbee93a9e966f7c67ba
Vladislav Mironchuk <vlad.mironchuk.02@gmail.com>
2022-01-20 11:13:03 +0300

    feature: small fixes

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,49 @@
 import "./styles/main.css";
 import "./styles/main.scss";
-import { StrictMode } from "react";
+import { StrictMode, Component } from "react";
 import ReactDom from "react-dom";
+import { Switch, Route, BrowserRouter, Redirect } from "react-router-dom";
+import { Layout } from "./components/layout";
+import { HomePage } from "./components/pages/homePage/homePage";
+import { AboutPage } from "./components/pages/aboutPage/aboutPage";
+import { ProductsPage } from "./components/pages/productsPage/productsPage";
+import { AuthPage } from "./components/pages/authPage/authPage";
 
-const AppComponent = () => {
-  return <h2>Staring new project...</h2>;
-};
+class AppComponent extends Component {
+  componentDidCatch() {
+    console.error("error");
+  }
+
+  render() {
+    return (
+      <Layout>
+        <Switch>
+          <Route path="/about">
+            <AboutPage />
+          </Route>
+          <Route path="/products/:category">
+            <ProductsPage />
+          </Route>
+          <Route path="/auth">
+            <AuthPage />
+          </Route>
+          <Route path="/">
+            <HomePage />
+          </Route>
+          <Route path="*">
+            <Redirect to="/" />
+          </Route>
+        </Switch>
+      </Layout>
+    );
+  }
+}
 
 ReactDom.render(
   <StrictMode>
-    <AppComponent />
+    <BrowserRouter>
+      <AppComponent />
+    </BrowserRouter>
   </StrictMode>,
   document.getElementById("app")
 );
--- webpack.mock.ts
+++ webpack.mock.ts
@@ -1,6 +1,58 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import webpackMockServer from "webpack-mock-server";
 
+const games = [
+  {
+    gameTitle: "Owerwatch",
+    gameLogo: "../../../assets/images/overwatch.jpg",
+    gamePrice: 23.99,
+    gamePlatforms: ["../../../assets/images/icone-windows-gris.png"],
+    gameDescription:
+      "Overwatch is a 2016 team-based multiplayer first-person shooter game developed and published by Blizzard Entertainment. Described as a 'hero shooter', Overwatch assigns players into two teams of six, with each player selecting from a large roster of characters, known as 'heroes', with unique abilities.",
+    ageLimit: 12,
+    rating: 5,
+    date: Date.now(),
+  },
+  {
+    gameTitle: "Minecraft",
+    gameLogo: "../../../assets/images/mine.jpg",
+    gamePrice: 25.99,
+    gamePlatforms: [
+      "../../../assets/images/icone-windows-gris.png",
+      "../../../assets/images/ps.png",
+      "../../../assets/images/xbox.png",
+    ],
+    gameDescription:
+      "Overwatch is a  team-based multiplayer first-person shooter game developed and published by Blizzard Entertainment. Described as a 'hero shooter', Overwatch assigns players into two teams of six, with each player selecting from a large roster of characters, known as 'heroes', with unique abilities.",
+    ageLimit: 12,
+    rating: 5,
+    date: Date.now(),
+  },
+  {
+    gameTitle: "Terraria",
+    gameLogo: "../../../assets/images/terraria.jpg",
+    gamePrice: 4.99,
+    gamePlatforms: [
+      "../../../assets/images/icone-windows-gris.png",
+      "../../../assets/images/ps.png",
+      "../../../assets/images/xbox.png",
+    ],
+    ageLimit: 12,
+    rating: 5,
+    date: Date.now(),
+    gameDescription:
+      "Terraria is a 2D sandbox game with gameplay that revolves around exploration, building, crafting, combat, survival, and mining, playable in both single-player and multiplayer modes. The game is noted for its classic exploration-adventure style of gameplay, similar to games such as the Metroid series and Minecraft.",
+  },
+  {
+    gameTitle: "CS:GO",
+    date: Date.now() - 1,
+  },
+  {
+    gameTitle: "CS 1.6",
+    date: Date.now() - 2,
+  },
+];
+
 export default webpackMockServer.add((app, helper) => {
   app.get("/testMock", (_req, res) => {
     const response = {
@@ -11,6 +63,20 @@ export default webpackMockServer.add((app, helper) => {
 
     res.json(response);
   });
+
+  app.get("/api/search/:text", (req, res) => {
+    if (req.params.text.trim() === "") {
+      res.json({ games: [] });
+    }
+    res.json({
+      games: games.map((game) => game.gameTitle).filter((game) => game.includes(req.params.text)),
+    });
+  });
+
+  app.get("/api/getTopProducts", (_req, res) => {
+    res.json({ games: games.sort((a, b) => b.date - a.date).slice(0, 3) });
+  });
+
   app.post("/testPostMock", (req, res) => {
     res.json({ body: req.body || null, success: true });
   });
