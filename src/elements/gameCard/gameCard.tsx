42db640abab28b5f161425a54d0a237b8ec271fc
Vladislav Mironchuk <vlad.mironchuk.02@gmail.com>
2022-01-20 11:08:20 +0300

    add: add game card

---
+++ src/elements/gameCard/gameCard.tsx
@@ -0,0 +1,41 @@
+import "./gameCard.scss";
+import star from "../../assets/images/star.png";
+
+export const GameCard: React.FC<{
+  gameLogo: string;
+  gameTitle: string;
+  gamePrice: number;
+  gamePlatforms: string[];
+  gameDescription: string;
+  ageLimit: number;
+  rating: number;
+}> = ({ gameLogo, gameTitle, gamePrice, gamePlatforms, gameDescription, ageLimit, rating }) => (
+  <div className="game-container">
+    <div className="game">
+      <div className="game__front">
+        <div className="game-platforms">
+          {gamePlatforms.map((platform) => (
+            <img key={Math.random()} className="game-platforms__logo" src={platform} />
+          ))}
+        </div>
+        <img className="game__front__logo" src={gameLogo} alt="" />
+        <p>
+          {gameTitle}
+          <span>{gamePrice}$</span>
+        </p>
+        <div className="rating__wrapper">
+          {[...new Array(rating)].map(() => (
+            <img key={Math.random()} className="rating" src={star} />
+          ))}
+        </div>
+      </div>
+      <div className="game__back">
+        <div className="game__back_wrapper">
+          <p className="game__back_wrapper_text">{gameDescription}</p>
+          <p className="game__back_wrapper_age">{ageLimit}+</p>
+          <button className="game__back_wrapper_button">Add to Cart</button>
+        </div>
+      </div>
+    </div>
+  </div>
+);
